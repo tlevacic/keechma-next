4d175856d1fd36a029f9d0e71951ff7b60197d3c
Tin August Levačić <tilevaci@vsite.hr>
2020-08-17 21:43:40 +0200

    Added comments to conformer.cljs

--- src/keechma/next/conformer.cljs
+++ src/keechma/next/conformer.cljs
@@ -4,29 +4,51 @@
 
 (declare conform-apps)
 
+;; PROVJERA KOJEGA JE TIPA KONTROLER
+;; Postoje 3 tipa kontrolera: factory (vector sa 1 elementom---) [:counter], :identity (vector sa 2 elementa, nastaje pomocu factory????) [:counter :1] , singleton-- :role
+;; Funkcija prima ime kontrolera i vraca tip kontrolera
+;; PRIMJER (get-controller-variant :role) ---> :singleton
 (defn get-controller-variant [controller-name]
   (cond
     (and (vector? controller-name) (= 2 (count controller-name))) :identity
     (and (vector? controller-name) (= 1 (count controller-name))) :factory
     :else :singleton))
 
+
+
+;; PROVERA DA LI SU PARAMSI CONTROLLERA DYNAMIC (DANA JE FUNKCIJA KOJOM SE ODREDUJE KADA SE KONTROLER PALI), STATIC
+;; Funkcija prima definiciju kontrolera i vraca kojeg je tipa params
+;; PRIMJER:
+;; (get-params-variant {:keechma.controller/params true
+;;                      :keechma.controller/type :keechma/entitydb}) --------> provjerava params i vraca :static
 (defn get-params-variant [controller-def]
   (if (fn? (:keechma.controller/params controller-def)) :dynamic :static))
 
-(defn conform-controller [[controller-name controller-def]]
-  (let [controller-variant (get-controller-variant controller-name)
-        params-variant    (get-params-variant controller-def)]
-    [controller-name
-     (cond-> controller-def
-       true (update :keechma.controller/type #(or % (if (vector? controller-name) (first controller-name) controller-name)))
-       true (assoc :keechma.controller/variant controller-variant)
-       (not= :factory controller-variant) (assoc :keechma.controller.params/variant params-variant))]))
 
+;;INICIJALIZACIJA KONTROLERA; VRACA [:ime-kontrolera {::type :singleton ::variant :static}]
+;;POSTAVLJA TIP I VARIJANTU PARAMSA
+(defn conform-controller [[controller-name controller-def]]
+  (let [controller-variant (get-controller-variant controller-name) ;;vraca singleton,  factory ili identity
+        params-variant    (get-params-variant controller-def)] ;; vraca dynamic ili static
+    [controller-name
+     (cond-> controller-def                           ;;cond-> stavlja controller-def na drugo mjesto kod izvrsavanja
+       true (update :keechma.controller/type #(or % (if (vector? controller-name) (first controller-name) controller-name))) ;;isto kao i (update controller-def :keechma.controller/type .....)
+       ;;gornji red ce updateat key ::type sa jednim imenom kontrolera, ukoliko je kontroler singleton, vratit ce isto :role, ukoliko je vektor [:counter] vratit ce :counter
+       true (assoc :keechma.controller/variant controller-variant) ;;postaviti ce ::variant na :dynamic ili :static
+       (not= :factory controller-variant) (assoc :keechma.controller.params/variant params-variant))])) ;;??????
+
+;;PRIMA VECTOR KONTROLERA I RADI GORNJU FUNKCIJU ZA SVAKI OD NJIH
+;; VRACA: {
+;;            [:ime-kontrolera {::type :singleton ::variant :static}]
+;;             [:ime-drugog-kontrolera {::type :factory ::variant :dynamic}]
+;;         }
 (defn conform-controllers [controllers]
   (->> controllers
     (map conform-controller)
     (into {})))
 
+
+;;ISTO KAO GORE SAMO ZA APP ???
 (defn conform-app [[app-name app-def]]
   (let [app-variant (if (contains? app-def :keechma.app/load) :dynamic :static)]
     [app-name
@@ -35,6 +57,7 @@
        (contains? app-def :keechma/controllers) (update :keechma/controllers conform-controllers)
        (contains? app-def :keechma/apps) (update :keechma/apps conform-apps))]))
 
+;;REGISTRIRA SVE APPOVE
 (defn conform-apps [apps]
   (->> apps
     (map conform-app)
